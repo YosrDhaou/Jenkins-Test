269f0d9544b658c5fea78ceaa96d18a7b48f6edc
YosrDhaou <dhaouyosr90@gmail.com>
2025-04-16 13:19:39 +0200

    Update JenkinsApplicationTests.java

--- src/test/java/com/example/demo/JenkinsApplicationTests.java
+++ src/test/java/com/example/demo/JenkinsApplicationTests.java
@@ -2,14 +2,15 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;  // Ajout de l'import pour assertEquals
 
 @SpringBootTest
 class JenkinsApplicationTests {
 
-	@Test
-	void contextLoads() {
-		System.out.println("it is a test class");
-		asserEquals (true , true);
-	}
-
+    @Test
+    void contextLoads() {
+        System.out.println("it is a test class");
+        assertEquals(true, true);  // Correction de la méthode ici
+    }
 }
+
